cffa80f5c95e2ceedc99676c044fcfa07c0e2c01
Whiting <whitinghtennis@gmail.com>
2021-08-30 09:35:44 -0700

    commit tree e9263b4059a155533ca4380e798f2cbdeb66e6e6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Bird.find_or_create_by(name: "American Robin", species: "Turdis migratorius")
+Bird.find_or_create_by(name: "Common Kingfisher", species: "Alcedo atthis")
+Bird.find_or_create_by(name: "Atlantic Puffin", species: "Fratercula arctica")
